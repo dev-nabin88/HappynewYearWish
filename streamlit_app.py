24d9521b31f0e6fb3c36e7037f44737eb44932b4
Nabin Ghorui <153352019+dev-nabin88@users.noreply.github.com>
2025-01-01 00:37:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import time
 from PIL import Image
 
-image=Image.open("C:\Users\ghoru\Downloads\WhatsApp Image 2024-12-31 at 23.25.20_1cea4870.jpg")
+image=Image.open("WhatsApp Image 2024-12-31 at 23.25.20_1cea4870.jpg")
 st.image(image)
 # Set page config for a modern look
 st.set_page_config(
